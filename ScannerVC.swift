6f8e964c70e9ed20581c4592b7d0cd0377103097
Kal <kal.aalrajhi@gmail.com>
2023-07-15 22:44:40 -0400

    Add ScannerVC class and import relevant modules

---
+++ ScannerVC.swift
@@ -0,0 +1,13 @@
+//
+//  ScannerVC.swift
+//  Barcode Scanner
+//
+//  Created by Dr Cpt Blackbeard on 7/15/23.
+//
+
+import UIKit
+import AVFoundation
+
+final class ScannerVC: UIViewController {
+    
+}

1f9ca0b999d78a4d136f1e0b081ad709f807344f
Kal <kal.aalrajhi@gmail.com>
2023-07-17 00:02:10 -0400

    consolidate alert structs into separate file

--- Barcode Scanner/BarcodeScannerView.swift
+++ Barcode Scanner/BarcodeScannerView.swift
@@ -7,24 +7,6 @@
 
 import SwiftUI
 
-struct AlertItem: Identifiable {
-    let id = UUID()
-    let title: String
-    let message: String
-    let dismissButton: Alert.Button
-}
-
-struct AlertContext {
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-                                              message: "We are unable to capture the input.",
-                                              dismissButton: .default(Text("Ok")))
-    
-    static let invalidScannedType = AlertItem(title: "Invalid Scan Type",
-                                              message: "Please use EAN-8 and EAN-13 barcode types.",
-                                              dismissButton: .default(Text("Ok")))
-}
-
-
 struct BarcodeScannerView: View {
     @State private var scannedCode = ""
     @State private var alertItem: AlertItem?

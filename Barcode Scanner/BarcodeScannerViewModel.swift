323db1b6374a062f9f1e848487df131386212fe4
Kal <kal.aalrajhi@gmail.com>
2023-07-17 00:13:08 -0400

    DRY up barcode scanner view logic and put into view model

--- Barcode Scanner/BarcodeScannerViewModel.swift
+++ Barcode Scanner/BarcodeScannerViewModel.swift
@@ -11,5 +11,11 @@ final class BarcodeScannerViewModel: ObservableObject {
     @Published var scannedCode = ""
     @Published var alertItem: AlertItem?
     
+    var statusText: String {
+        return scannedCode.isEmpty ? "Not Yet Scanned" : scannedCode
+    }
     
+    var statusTextColor: Color {
+        return scannedCode.isEmpty ? .red : .green
+    }
 }
